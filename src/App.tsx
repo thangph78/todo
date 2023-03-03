e827762a79cbd9ce79c34fda16d277689d01da63
thangph78 <thangph78@gmail.com>
2023-03-04 03:48:05 +0700

    Todo app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { Button, Col, FormGroup, ListGroup, Row } from 'react-bootstrap'
+import Form from 'react-bootstrap/Form';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+
+export default function App() {
+
+  const [age, setAge] = useState(25)
+  const [name, setName] = useState("Thang Simon")
+  const [career, setCareer] = useState("Developer")
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Form style={{padding: '100px'}}>
+      <Row>
+        <Col>
+          {/* Input value */}
+          <h1>FORM</h1>
+          <Form>
+            <FormGroup className='mb-3'>
+              <Form.Control type="text" value={name} placeholder="Name" />
+            </FormGroup>
+            <FormGroup className='mb-3'>
+              <Form.Control type="age" value={age} placeholder="Age" />
+            </FormGroup>
+            <FormGroup className='mb-3'>
+              <Form.Control as="textarea" value={career} placeholder="Career" />
+            </FormGroup>
+          </Form>
+          <Button variant='danger'>Submit</Button>
+        </Col>
+          
+        {/* List Group */}
+        <Col>
+          <ListGroup>
+           <h1>LIST</h1>
+            <ListGroup.Item>
+            {name} - {age} - {career}
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+      </Row>
+    </Form>
+  )
 }
-
-export default App;
